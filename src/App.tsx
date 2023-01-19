2f4bd3362789daf026d8aebf19f91a84283193f0
Vera Krasnova <azirafail@mail.ru>
2023-01-19 17:19:10 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { CardPage } from 'pages/CardPage';
+import { ListPage } from 'pages/ListPage';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="app">
-      <p>TP LAB TEST</p>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<ListPage />} />
+        <Route path="card/:id" element={<CardPage />} />
+      </Routes>
+    </>
   );
 }
 
