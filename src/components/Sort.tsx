00bdfe5cb7ded479ff8b3976b1cad30dd64d69b3
Vera Krasnova <azirafail@mail.ru>
2023-01-20 15:07:14 +0300

    feat: add sortings layout

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
+import { useState } from 'react';
 
 export const Sort = () => {
+  const [activeSortParam, setActiveSortParam] = useState(0);
+  const [isAscSort, setAscSort] = useState(true);
+
+  const sortParams = ['по названию', 'по просмотрам', 'по дате начала', 'по дате окончания'];
+
+  const sortParamClickHandler = (index: number) => {
+    if (index === activeSortParam) {
+      setAscSort(!isAscSort);
+    }
+    setActiveSortParam(index);
+  };
+
+  const getClassName = (index: number) => {
+    if (activeSortParam === index && isAscSort) {
+      return 'sort__item_active asc';
+    } else if (activeSortParam === index && !isAscSort) {
+      return 'sort__item_active desc';
+    } else {
+      return '';
+    }
+  };
+
   return (
     <div className="sort">
       <span>Сортировать:</span>
-      <span className="sort__item">по названию</span>
-      <span className="sort__item">по просмотрам</span>
-      <span className="sort__item">по дате начала</span>
-      <span className="sort__item">по дате окончания</span>
+      {sortParams.map((param, index) => {
+        return (
+          <span
+            className={`sort__item ${getClassName(index)}`}
+            key={param}
+            onClick={() => sortParamClickHandler(index)}>
+            {param}
+          </span>
+        );
+      })}
     </div>
   );
 };
