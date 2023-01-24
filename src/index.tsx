596bd81a687b12ec13508f087f726091982fb4c3
Vera Krasnova <azirafail@mail.ru>
2023-01-24 23:51:36 +0300

    feat: implement saving state to local storage with redux-persist package

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './styles/styles.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store';
+import store, { persistor } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,50 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import paginationReducer from './pagination/slice';
 import filtersReducer from './filter/slice';
 import productsReducer from './products/slice';
 import langReducer from './lang/slice';
 import { useDispatch } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: {
-    pagination: paginationReducer,
-    filters: filtersReducer,
-    products: productsReducer,
-    lang: langReducer,
-  },
+const rootReducer = combineReducers({
+  pagination: paginationReducer,
+  filters: filtersReducer,
+  products: productsReducer,
+  lang: langReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
 type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
+
+export default store;
