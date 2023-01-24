0a4d120436d3ff36c300e9abd8043c703769dda7
Vera Krasnova <azirafail@mail.ru>
2023-01-25 01:30:30 +0300

    readme: update

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/tp-lab-test">
+      <BrowserRouter>
         <PersistGate loading={null} persistor={persistor}>
           <App />
         </PersistGate>
