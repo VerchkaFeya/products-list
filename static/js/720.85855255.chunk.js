78dd0035ffc7e15eba7a454bbd3cae1b92feb918
Vera Krasnova <azirafail@mail.ru>
2023-01-24 23:11:03 +0300

    Updates

---
+++ static/js/720.85855255.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktp_lab_test=self.webpackChunktp_lab_test||[]).push([[720],{720:function(t,e,n){n.r(e);n(2791);var s=n(184);e.default=function(){return(0,s.jsx)("div",{className:"not-found-page",children:"\u0421\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430"})}}}]);
+//# sourceMappingURL=720.85855255.chunk.js.map
